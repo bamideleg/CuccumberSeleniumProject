7132416ab8cf4967b7fcf9def36902ffff39330c
bamideleg <bamideleg74@gmail.com>
2021-11-17 18:20:11 +0000

    Commit  Work on the feature-branch 2.1 - added multiple search functionality

--- src/test/java/Test_Execution/EbayEndToEnd_Search.java
+++ src/test/java/Test_Execution/EbayEndToEnd_Search.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 
-public class EbayHomepagelogo {
+public class EbayEndToEnd_Search {
 
     WebDriver driver;
 
@@ -37,7 +37,7 @@ public class EbayHomepagelogo {
 // Work on the feature-branch 2.1 - Add search functionality code
     @When("I enter product name as shirt")
     public void i_enter_product_name_as_shirt() {
-       driver.findElement(By.name("_nkw")).sendKeys("book");
+       driver.findElement(By.name("_nkw")).sendKeys("Shirt");
        driver.findElement(By.id("gh-btn")).click();
     }
     @Then("system should display item_Count")
@@ -47,6 +47,22 @@ public class EbayHomepagelogo {
     }
     @Then("close the browser2")
     public void close_the_browser2() {
+        driver.close();
+    }
+
+    @When("I enter more than one product as {string}")
+    public void i_enter_more_than_one_product_as(String Product) {
+        driver.findElement(By.name("_nkw")).sendKeys(Product);
+        driver.findElement(By.id("gh-btn")).click();
+
+    }
+    @Then("system should display items count for each product")
+    public void system_should_display_items_count_for_each_product() {
+        String productCount =driver.findElement(By.cssSelector(".srp-controls__count-heading")).getText();
+        System.out.println(" The item count is " + productCount);
+    }
+    @Then("close the browser3")
+    public void close_the_browser3() {
         driver.close();
     }
 }
